8f80832c6df33ede8a76980ba9d8e31ec9acda00
KS-Abhi <93191466+KS-Abhi@users.noreply.github.com>
2021-11-15 19:07:49 +0530

    change

--- signup.js
+++ signup.js
@@ -13,40 +13,40 @@ function validate(){
        error.innerHTML = "valid email format";
        error.style.color = "green";
        var lengthy = password.value.length;
-       if(numberreg.test(number.value)){
-           error2.innerHTML="Valid Number format";
-           error2.style.color="green";
-       if(regpwd.test(password.value)){
-           if(lengthy<10){
-               error1.innerHTML = "password strength: Poor";
-               error1.style.color = "red";
-               return true;
-           }
-           else if(lengthy<13){
-               error1.innerHTML = "Password strength: Medium";
-               error1.style.color = "orange";
-               return true;
-           }
-           else{
-               error1.innerHTML = "Password strength: Strong";
-               error1.style.color = "green";
-               return true;
-           }
-       }
-       else{
-           error1.innerHTML = "Invalid Password format";
-           error1.style.color = "red";
-           return false;
-       }
+       if(regnumber.test(number.value)){
+           error1.innerHTML="Valid Number format";
+           error1.style.color="green";
+    //    if(regpwd.test(password.value)){
+    //        if(lengthy<10){
+    //            error1.innerHTML = "password strength: Poor";
+    //            error1.style.color = "red";
+    //            return true;
+    //        }
+    //        else if(lengthy<13){
+    //            error1.innerHTML = "Password strength: Medium";
+    //            error1.style.color = "orange";
+    //            return true;
+    //        }
+    //        else{
+    //            error1.innerHTML = "Password strength: Strong";
+    //            error1.style.color = "green";
+    //            return true;
+    //        }
+    //    }
+    //    else{
+    //        error1.innerHTML = "Invalid Password format";
+    //        error1.style.color = "red";
+    //        return false;
+    //    }
    }
    else{
-       error2.innerHTML="Invalid Number format";
-       error2.style.color="red";
+       error1.innerHTML="Invalid Number format";
+       error1.style.color="red";
        return false;
    }
    }
    else{
-       error.innerHTML = "invalid, please enter a valid email";
+       error.innerHTML = "Invalid!! please enter a valid email";
        error.style.color = "red";
        return false;
    }
