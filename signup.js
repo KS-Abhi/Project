8aecee0c73b55409df5ca42e91310f27dcf4fc61
KS-Abhi <93191466+KS-Abhi@users.noreply.github.com>
2021-11-15 19:23:07 +0530

    change

--- signup.js
+++ signup.js
@@ -1,43 +1,47 @@
 function validate(){
    
    let email = document.getElementById("email");
-   let number = document.getElementById("number");
+   let number = document.getElementById("phone");
    let password = document.getElementById("password");
-   let cpassword = document.getElementById("password2")
+
 
 
    let regemail=/^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/;
    let regpwd = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
    let regnumber = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+   
+   
    if(regemail.test(email.value)){
        error.innerHTML = "valid email format";
        error.style.color = "green";
-       var lengthy = password.value.length;
+       
+       
+    var lengthy = password.value.length;
        if(regnumber.test(number.value)){
            error1.innerHTML="Valid Number format";
            error1.style.color="green";
-    //    if(regpwd.test(password.value)){
-    //        if(lengthy<10){
-    //            error1.innerHTML = "password strength: Poor";
-    //            error1.style.color = "red";
-    //            return true;
-    //        }
-    //        else if(lengthy<13){
-    //            error1.innerHTML = "Password strength: Medium";
-    //            error1.style.color = "orange";
-    //            return true;
-    //        }
-    //        else{
-    //            error1.innerHTML = "Password strength: Strong";
-    //            error1.style.color = "green";
-    //            return true;
-    //        }
-    //    }
-    //    else{
-    //        error1.innerHTML = "Invalid Password format";
-    //        error1.style.color = "red";
-    //        return false;
-    //    }
+         if(regpwd.test(password.value)){
+           if(lengthy<10){
+               error2.innerHTML = "password strength: Poor";
+               error2.style.color = "red";
+               return true;
+           }
+           else if(lengthy<13){
+               error2.innerHTML = "Password strength: Medium";
+               error2.style.color = "orange";
+               return true;
+           }
+           else{
+               error2.innerHTML = "Password strength: Strong";
+               error2.style.color = "green";
+               return true;
+           }
+       }
+       else{
+           error2.innerHTML = "Invalid Password format";
+           error2.style.color = "red";
+           return false;
+       }
    }
    else{
        error1.innerHTML="Invalid Number format";
